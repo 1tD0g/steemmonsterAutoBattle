b911f93bf030ee0c695db740b1cd5e6ae5b1c457
itD0g <itD0g>
2018-10-28 14:32:16 +0800

    init commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# Auto Battle in Steemmonster
+
+Auto battle in steemmonster, The script will battle automatically using the same team.
+
+## Author
+
+[My Steemit](https://steemit.com/@itdog)
+
+## Requirements:
+
+1. Tampermonkey
+2. Firefox / Chrome
+
+## How to use it
+
+**Remember to choose the team you are gonna use before running the script**
+
+1. Paste the script to Tampermonkey and save it
+
+2. Make sure you chose the team you are gonna use before enable the script.
+
+3. enable the script on Tampermonkey and refresh the site
+
+4. Play "Ranked" or "Partice" in Steemmonster
+
+5. The Script will start when it can find the **Battle** Button in the page.(So you don't have time to choose team or monster when you started the script.)
+
+6. The Script will keep running no matter you lose or win the game
+
+**To stop the script just disable it on Tampermonkey and refresh the page**
+
+**Will update later when I have time, maybe make a UI or record how many games you lost or won.**
---
+++ steemmoster.js
@@ -0,0 +1,77 @@
+// ==UserScript==
+// @name         Steem Monster Auto Battle
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Battle in Steem Monster automatically
+// @author       1tD0g
+// @match        https://steemmonsters.com/
+// @grant        none
+// ==/UserScript==
+
+(async function() {
+    'use strict';
+    let running = false;
+    setInterval(async () => {
+        if(!running){
+            try{
+                console.log("Starting Script...");
+                running = true;
+                await waitUntilBattleButtonAndClick(200);
+                await waitUntilBattleOpponentFound(2000);
+                await waitX(2000);
+                startFightLoop();
+                await waitX(2000);
+                btnSkipClick();
+                await waitX(2000);
+                await waitUntilBattleAgainAndClick();
+                running = false;
+                console.log("1 Round Finished")
+            }catch(e){
+                console.log("Error from Script: ", e);
+            }
+       }
+    }, 5000);
+})();
+
+function waitX(x){
+    return new Promise((resolve, reject) => {
+        setTimeout(resolve, x)
+    })
+}
+
+function waitUntilBattleButtonAndClick(x){
+    return new Promise((resolve, reject) => {
+        let checkInterval = setInterval(() => {
+            let battleButtons = document.getElementsByClassName("battle-btn");
+            if(battleButtons == null) return;
+            if(battleButtons.length < 1) return;
+            clearInterval(checkInterval);
+            battleButtons[0].click();
+            return resolve();
+        }, x)
+    })
+}
+
+function waitUntilBattleOpponentFound(x){
+    return new Promise((resolve, reject) => {
+        let intervv = setInterval(() => {
+            let dialog = document.getElementById("find_opponent_dialog");
+            if(dialog != null && dialog.style.display == "none"){
+                clearInterval(intervv);
+                return resolve();
+            }
+        }, x)
+    })
+}
+
+function waitUntilBattleAgainAndClick(x){
+    return new Promise((resolve, reject) => {
+        let checkInterval = setInterval(() => {
+            let battleAgain = document.getElementById("btn_again");
+            if(battleAgain == null) return;
+            clearInterval(checkInterval);
+            battleAgain.click();
+            return resolve();
+        }, x)
+    })
+}
